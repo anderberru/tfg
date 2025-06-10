805aa929549f595fa0dcc9b5df00d8c7cf4ed77e
Ander Berruezo Aguirre <berru0429@gmail.com>
2025-06-10 16:57:48 +0200

    update setup script

--- public/javascripts/tensorflow_install.js
+++ public/javascripts/tensorflow_install.js
@@ -1,9 +1,25 @@
 const { execSync } = require("child_process");
 
-// https://drive.google.com/file/d/1k7QlhI7fNGt5UKDAiGNuiYbZ_oJ5qjS2/view?usp=sharing
+function getPythonCmd() {
+  try {
+    execSync("python --version", { stdio: "ignore" });
+    return "python";
+  } catch {
+    try {
+      execSync("python3 --version", { stdio: "ignore" });
+      return "python3";
+    } catch {
+      throw new Error("Neither 'python' nor 'python3' is available on this system.");
+    }
+  }
+}
+
+const pythonCmd = getPythonCmd();
 const fileId = "1k7QlhI7fNGt5UKDAiGNuiYbZ_oJ5qjS2";
 const output = "vagrant/programs/tensorflow-2.7.1-cp39-cp39-linux_x86_64.whl";
 
-console.log("Descargando archivo desde Google Drive...");
-execSync("pip install --quiet gdown", { stdio: "inherit" });
-execSync(`gdown https://drive.google.com/uc?id=${fileId} -O ${output}`, { stdio: "inherit" });
+console.log("Installing gdown...");
+execSync(`${pythonCmd} -m pip install --upgrade --quiet gdown`, { stdio: "inherit" });
+
+console.log("Downloading tensorflow.whl file from Google Drive...");
+execSync(`${pythonCmd} -m gdown https://drive.google.com/uc?id=${fileId} -O ${output}`, { stdio: "inherit" });
